16805578b7e2beaa3eb99c16b76f79a0fd1e0e58
heiyedeshengyin <42495512+heiyedeshengyin@users.noreply.github.com>
2020-03-12 23:30:54 +0800

    Create SqlSession.java

---
+++ src/main/java/com/hjr/mybatis/sqlsession/SqlSession.java
@@ -0,0 +1,18 @@
+package com.hjr.mybatis.sqlsession;
+
+/**
+ * 通过动态代理返回实现dao接口的对象<br>
+ * 代理模式<br>
+ * 不改变源码的情况对已有方法进行增强
+ */
+public interface SqlSession {
+    /**
+     * 返回实现dao接口的对象
+     * @param daoInterfaceClass dao接口字节码
+     * @param <T> dao接口类型
+     * @return dao接口实现对象
+     */
+    <T> T getMapper(Class<T> daoInterfaceClass);
+
+    void close();
+}
