c2d4610c47c646eabb5e176de842d05deb84a8c6
heiyedeshengyin <42495512+heiyedeshengyin@users.noreply.github.com>
2020-03-12 23:22:05 +0800

    Create MybatisTest.java

---
+++ src/test/java/com/hjr/test/MybatisTest.java
@@ -0,0 +1,40 @@
+package com.hjr.test;
+
+import com.hjr.dao.UserDao;
+import com.hjr.domain.User;
+import com.hjr.mybatis.io.Resources;
+import com.hjr.mybatis.sqlsession.SqlSession;
+import com.hjr.mybatis.sqlsession.SqlSessionFactory;
+import com.hjr.mybatis.sqlsession.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisTest {
+    @Test
+    public void test1() throws IOException {
+        //1.读取配置文件
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+
+        //2.创建SqlSessionFactory工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(in);
+
+        //3.使用工厂生产SqlSession对象
+        SqlSession session = factory.openSession();
+
+        //4.使用SqlSession创建Dao接口的代理对象
+        UserDao userDao = session.getMapper(UserDao.class);
+
+        //5.使用代理对象执行方法
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        session.close();
+        in.close();
+    }
+}
